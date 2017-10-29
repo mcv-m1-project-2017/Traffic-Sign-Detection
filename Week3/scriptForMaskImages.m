379bef10e8f21a91d989ab66f9a3c663eaec8adb
willytell <guille.torres@gmail.com>
2017-10-29 13:21:11 +0100

    Week3, it generates a window candidate .mat for each mask

--- Week3/scriptForMaskImages.m
+++ Week3/scriptForMaskImages.m
@@ -10,29 +10,28 @@ end
 model = 'HSV';
 % Possible models to choose: {'CCL','Sliding Window','Integral Image','Convolution'}
 regionModel = 'Convolution';
-[TestMasks,windowCandidates] = maskGeneratorMM(pathToDir,Test,model,regionModel);
+[TestMasks,BoundingBoxes] = maskGeneratorMM(pathToDir,Test,model,regionModel);
 
-strct = [];
-
-for i=1:numel(windowCandidates)
-    mat = windowCandidates{i};
-    if ~isempty(mat)
-        for j=1:size(mat,1)
-            strct = [strct ; struct('x', mat(j,1), 'y', mat(j,2), 'w', mat(j,3), 'h', mat(j,4))];
-        end
-    end
-end
-
-windowCandidates = strct;
-
-%
 % It's supposed to be a foder named 'resultsTests'
  for i=1:size(TestMasks,3)
     name = strcat('mask.', testEls(i).name(1:end-4));
-%     cad = strcat('../method1/',  name, '.png');
     imwrite(uint8(TestMasks(:,:,i)),['../Method 4/' name '.png'])
+    mat = BoundingBoxes{i};
+    strct = [];
+    if ~isempty(mat)
+        for j=1:size(mat,1)
+            strct = [strct;struct('x', mat(j,1), 'y', mat(j,2), 'w', mat(j,3), 'h', mat(j,4))];
+        end
+    else
+       strct.x = [];
+       strct.y = [];
+       strct.w = [];
+       strct.h = [];
+    end
+    windowCandidates = strct;
+    save(['../Method 4/' name '.mat'], 'windowCandidates')
  end
  
- save ../'Method 4'/windowCandidates.mat windowCandidates
+%  save ../'Method 4'/windowCandidates.mat windowCandidates
  
  
